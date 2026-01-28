9122b349d1a610a9e0dd30d3fe1f32dcbb2a79bb
alpredoruben <alpredo.tampubolon@gmail.com>
2026-01-28 12:04:35 +0700

    [create] - Setup folder structured. Implementasi dari Layered Arsitektur

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/migrations/**',
+    '!src/seeders/**',
+    '!src/server.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/repositories/$1',
+    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@validators/(.*)$': '<rootDir>/src/validators/$1',
+    '^@events/(.*)$': '<rootDir>/src/events/$1',
+    '^@listeners/(.*)$': '<rootDir>/src/listeners/$1',
+    '^@queues/(.*)$': '<rootDir>/src/queues/$1',
+    '^@jobs/(.*)$': '<rootDir>/src/jobs/$1',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        esModuleInterop: true,
+      },
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testTimeout: 10000,
+  verbose: true,
+};
